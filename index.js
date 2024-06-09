b5a50a46351c546e613d9daafd936fbbee4ea095
SantoSarker101 <santosarker404@gmail.com>
2024-06-09 10:18:44 +0600

    Created jwt axios and payment system

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const nodemailer = require('nodemailer')
 const port = process.env.PORT || 5000
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 
 // middleware
 const corsOptions = {
@@ -12,6 +16,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(morgan('dev'))
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
@@ -26,11 +31,87 @@ const client = new MongoClient(uri, {
   },
 })
 
+// Verify/Validate JWT
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization
+  // console.log(authorization);
+  if(!authorization){
+    return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+  }
+  const token = authorization.split(' ')[1]
+  console.log(token);
+  // Token Verify
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+
+}
+
+
+// send mail function
+const sendMail = (emailData, emailAddress) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: emailAddress,
+    subject: emailData.subject,
+    html: `<p>${emailData?.message}</p>`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+   console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      // do something useful
+    }
+  });
+}
+
+
 async function run() {
   try {
     const usersCollection = client.db('airbnbDb').collection('users')
     const roomsCollection = client.db('airbnbDb').collection('rooms')
     const bookingsCollection = client.db('airbnbDb').collection('bookings')
+
+
+    // Generate Client Secret
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      const {price} = req.body
+      // console.log(price);
+      if(price){
+        const amount = parseFloat(price) * 100
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        })
+        res.send({ clientSecret: paymentIntent.client_secret })
+      }
+    })
+
+
+    // Generate jwt token
+    app.post('/jwt', (req, res) => {
+      const email = req.body
+      // console.log(email);
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '900000000d'})
+      // console.log(token);
+      res.send({token})
+    })
+
 
 
     // Save User email and role in DB
@@ -53,7 +134,7 @@ async function run() {
       const email = req.params.email
       const query = { email: email }
       const result = await usersCollection.findOne(query)
-      console.log(result);
+      // console.log(result);
       res.send(result)
     })
 
@@ -72,18 +153,24 @@ async function run() {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await roomsCollection.findOne(query)
-      console.log(result);
+      // console.log(result);
       res.send(result)
     })
 
 
 
     // Get filtered rooms for hosts
-    app.get('/rooms/:email', async (req, res) => {
+    app.get('/rooms/:email', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email
+      console.log(decodedEmail);
       const email = req.params.email
+      if(email !== decodedEmail){
+        return res.status(403).send({ error: true, message: 'Forbidden Access' })
+      }
+
       const query = { 'host.email': email }
       const result = await roomsCollection.find(query).toArray()
-      console.log(result);
+      // console.log(result);
       res.send(result);
     })
 
@@ -92,9 +179,9 @@ async function run() {
     // Save a room in database
     app.post('/rooms', async (req, res) => {
       const room = req.body;
-      console.log(room);
+      // console.log(room);
       const result = await roomsCollection.insertOne(room)
-      console.log(result);
+      // console.log(result);
       res.send(result)
     })
 
@@ -125,6 +212,21 @@ async function run() {
 
 
 
+    // Update a room in Database
+    app.put('/rooms/:id', verifyJWT, async (req, res) => {
+      const room = req.body
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: room,
+      }
+      const result = await roomsCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
+    })
+
+
+
 
     // Get Bookings for Guest
     app.get('/bookings', async (req, res) => {
@@ -140,12 +242,41 @@ async function run() {
 
 
 
+    // Get Bookings for Host
+    app.get('/bookings/host', async (req, res) => {
+      const email = req.query.email
+
+      if(!email){
+        res.send
+      }
+      const query = { host: email }
+      const result = await bookingsCollection.find(query).toArray()
+      res.send(result);
+    })
+
+
+
     // Save a Booking in Database
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
       console.log(booking);
       const result = await bookingsCollection.insertOne(booking)
-      console.log(result);
+      // console.log(result);
+      sendMail({
+        subject: 'Booking Successful',
+        message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`,
+      },
+      booking?.guest?.email
+    )
+
+
+    sendMail({
+      subject: 'Your Room got booked',
+      message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`,
+    },
+    booking?.host
+  )
+
       res.send(result)
     })
 
